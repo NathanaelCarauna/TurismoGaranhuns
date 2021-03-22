5379be67cfa2ec6fa8aa9867d3e35f8f1ce2fb3b
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-03-21 23:53:41 -0300

    Storing user authentication data

--- client/src/contexts/auth.js
+++ client/src/contexts/auth.js
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import * as auth from '../services/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AuthContext = createContext({
     signed: false,
@@ -11,9 +12,23 @@ const AuthContext = createContext({
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
+    useEffect(() => {
+        async function loadStorageData(){
+            const storagedUser = await AsyncStorage.getItem('@TGAuth:user');
+            const storageToken = await AsyncStorage.getItem('@TGAuth:token');
+
+            if(storagedUser && storageToken){
+                setUser(JSON.parse(storagedUser));
+            }
+        }
+        loadStorageData();
+    }, [])
     async function signIn() {
         const response = await auth.signIn();        
         setUser(response.user);
+
+        await AsyncStorage.setItem('@TGAuth:user', JSON.stringify(response.user));
+        await AsyncStorage.setItem('@TGAuth:token', response.token);
     }
 
     async function signOut() {
