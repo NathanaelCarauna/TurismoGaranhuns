8b4d59d258528044bf64fc95ed435f8d64132139
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-01 16:54:16 -0300

    Methods no related removed

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,10 +1,6 @@
 import api from './api';
 
 export function signIn(email, password){
-    return new api.post('/signin', {email, password})        
-        
+    return new api.post('/signin', {email, password})                
 }
 
-export function signUp(nome, email, password){
-    return new api.post('/signup', {nome, email, password})
-}
