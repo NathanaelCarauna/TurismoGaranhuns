a5f135753216f1223336b9091726a3b00eef42e1
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-01 11:20:57 -0300

    Update and remove user added to routes

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,13 @@
 module.exports = app => {
+    //USER API
     app.post('/signup', app.api.user.save);
     app.post('/signin', app.api.auth.signin);
+    
+    app.route('/updateUser/:id')
+        .all(app.config.passport.authenticate())
+        .patch(app.api.user.updateUser)
+    
+    app.route('/removeUser/:id')
+        .all(app.config.passport.authenticate())
+        .delete(app.api.user.removeUser)
 }
