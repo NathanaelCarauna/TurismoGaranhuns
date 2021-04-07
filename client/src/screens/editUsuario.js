35c9226b24620757875950b83b0382efd0f38831
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-07 10:51:37 -0300

    EditUser screen prototype created

--- client/src/screens/editUsuario.js
+++ client/src/screens/editUsuario.js
@@ -1,14 +1,132 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, {useState, useContext} from 'react';
+import {
+Alert,
+StyleSheet,
+Text,
+View,
+FlatList,
+TouchableOpacity,
+TextInput,
+} from "react-native";
+import AuthContext from '../contexts/auth';
 
-export default function EditUsuario({navigation}){
+export default function EditUsuario({ navigation }) {
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const { user } = useContext(AuthContext);
     return (
-        <View style={{flex:1}}>
-            <View style={{flexDirection:'row',justifyContent:'space-between'}}>
-                <Button title ='Abrir'
-                        onPress={() => navigation.openDrawer()}/>
-            </View>
+        <View style={styles.container}>    
+            <View style={styles.upperSide}>
+            
+            </View>        
+            <View style={styles.formContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite seu nome"
+                    value={user.nome}
+                    onChangeText={nome => setNome(nome)} />
 
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite seu email"
+                    value={user.email}
+                    onChangeText={email => setEmail(email)} />
+
+                <TextInput
+                    style={styles.input}
+                    secureTextEntry={true}
+                    placeholder="Digite uma senha"
+                    value={password}
+                    onChangeText={password => setPassword(password)} />
+
+                <TextInput
+                    style={styles.input}
+                    secureTextEntry={true}
+                    placeholder="Confirme sua senha"
+                    value={confirmPassword}
+                    onChangeText={confirmPassword => setConfirmPassword(confirmPassword)} />
+
+                <TouchableOpacity onPress={()=>{}}>
+                    <View style={styles.botaoEntrar}>
+                        <Text style={styles.botaoText}>Salvar</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    // Main start
+    container: {
+      flex: 1,    
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    upperSide: {
+        flex: 1,
+        backgroundColor: "white",
+        width: "100%",
+    },
+    formContainer: {
+        flex: 2,
+        backgroundColor: "#bbb",
+        paddingTop: 20,
+        width: "100%",
+        alignItems: "center",
+    },
+    input: {
+      marginTop: 10,
+      padding: 10,
+      width: 300,
+      backgroundColor: "#fff",
+      fontSize: 16,
+      fontWeight: "bold",
+      borderRadius: 3,
+    },
+    botao: {
+      width: 300,
+      height: 42,
+      backgroundColor: "#fff",
+      marginTop: 10,
+      borderRadius: 4,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    botaoText: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "black",
+    },
+    input: {
+      marginTop: 10,
+      padding: 10,
+      width: 300,
+      backgroundColor: "#fff",
+      fontSize: 16,
+      fontWeight: "bold",
+      borderRadius: 3,
+    },
+    botaoEntrar: {
+      width: 300,
+      height: 42,
+      backgroundColor: "#213967",
+      textDecorationColor: "#fff",
+      marginTop: 30,
+      borderRadius: 4,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    logo: {
+      width: 80,    
+      height: 80,
+      marginBottom: 35,
+    },    
+    botaoText: {
+        color: "#FFFFFF",
+    }
+  });
+  
