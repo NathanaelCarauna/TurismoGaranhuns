dda214f8d9d2587cca5bc3f1650916f0e4ea5bfb
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-04-25 16:11:25 -0300

    Bug header fixed

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -32,7 +32,7 @@ module.exports = app => {
             })
         }
         else{
-            return res.json({user: null}).send("Usuário não cadastrado")
+            return res.json({user: null})
         }
     }
     return { signin }
